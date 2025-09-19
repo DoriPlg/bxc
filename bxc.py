e316d1883fe8e5d2fadd3e1cea2490ac0868cbef
DoriPlg <1dor.peleg@gmail.com>
2025-09-19 10:25:11 +0200

    Sleek Sleek oneliners

--- bxc.py
+++ bxc.py
@@ -2,37 +2,51 @@
 A simple driver program to test the bx parser and AST construction."""
 
 import sys
+import json
 from bxast import *
 from bxparser import Parser
 from bxmunch import *
 from bxerrors import DefaultReporter
 
-def _test_parser():
+def _run_parser():
     if len(sys.argv)-1 < 1:
         print(f"Usage: {sys.argv[0]} [filename.bx...]", file = sys.stderr)
         exit(1)
     filename = sys.argv[2] if len(sys.argv) > 2 else sys.argv[1]
+    outfile = f"out/{filename.split('.')[0].split('/')[-1]}.tac.json"
     with open(filename, "r", encoding="UTF-8") as stream:
         contents = stream.read()
+
     reporter = DefaultReporter(source = contents)
+    tree = parse(contents, reporter)
+
+    munch_type = sys.argv[1] if len(sys.argv) > 2 else "--tmm"
+    mm = make_muncher(tree, reporter, munch_type)
+
+    munch_gen_json(mm, outfile)
+    sys.exit(0 if reporter.nerrors == 0 else 1)
+
+def parse(contents: str, reporter: Reporter):
+    """Parse the given program contents and return the AST.
+    If there are any errors, report them using the given reporter and return exit.
+    """
     tree = Parser(reporter).parse(contents)
     if reporter.nerrors:
         sys.exit(1)
-    munch_type = sys.argv[1] if len(sys.argv) > 2 else "--tmm"
-    if munch_type == "--tmm":
-        mm = TopDownMunch(tree)
-    elif munch_type == "--bmm":
-        mm = BottomUpMunch(tree)
-    else:
+    return tree
+
+def make_muncher(tree: AST, reporter: Reporter, munch_type: str) -> Munch:
+    mm = TopDownMunch(tree, reporter) if munch_type == "--tmm" else \
+        BottomUpMunch(tree, reporter) if munch_type == "--bmm" else  None
+    if mm is None:
         print(f"Unknown muncher type '{munch_type}'", file = sys.stderr)
         sys.exit(1)
-    outfile = f"out/{filename.split('.')[0].split('/')[-1]}.tac.json"
+    return mm
+
+def munch_gen_json(mm: Munch, outfile: str):
     with open(outfile, "w", encoding="UTF-8") as outfile:
-        import json
         json.dump(mm.munch(), outfile, indent=4)
     print(f"Wrote TAC to {outfile.name}")
-    sys.exit(0)
 
-    
 if __name__=="__main__":
-    _test_parser()
+    _run_parser()
